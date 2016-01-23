386a40bd381d63606a8cf51efd887c3d2df66545
thongnv <thongola@hotmail.com>
2016-01-23 15:42:39 +0700

    completed the view for gallery

---
+++ d_web/gallery/static/gallery/js/gallery.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+  $('#slide').bjqs({
+    keyboardnav : true, // enable keyboard navigation
+    hoverpause : true, // pause the slider on hover
+    automatic  : true,
+//    height      : 320,
+//    width       : 620,
+//    responsive  : true
+
+  });
+
+});
