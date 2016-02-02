4c73552dff1546c2ff3613ef32468a025fea832f
thongnv <thongola@hotmail.com>
2016-02-02 23:45:17 +0700

    little fix on gallery home screen

--- d_web/gallery/static/gallery/js/gallery.js
+++ d_web/gallery/static/gallery/js/gallery.js
@@ -1,17 +1,9 @@
 jQuery(document).ready(function($) {
 
-    $('#thumbList').justifiedGallery({
-			rowHeight : 120,
-			fixedHeight : false,
-			captions : false,
-			margins : 7,
-			sizeRangeSuffixes: {
-				'lt100':'_t',
-				'lt240':'_m',
-				'lt320':'_n',
-				'lt500':'',
-				'lt640':'_z',
-				'lt1024':'_b'
-			}
-        })
+    $('#slide').justifiedGallery({
+        rowHeight : 300,
+        fixedHeight : false,
+        captions : false,
+        margins : 7,
+    })
 });
